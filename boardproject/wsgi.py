078db98d758c49d5559b6311fbf6fc1dbf145249
Akiyoshi999 <zundokokiyoshi999@gmail.com>
2020-11-19 08:10:44 +0900

    deploy2

--- boardproject/wsgi.py
+++ boardproject/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boardproject.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+
+from django.core.wsgi import get_wsgi_application
+
